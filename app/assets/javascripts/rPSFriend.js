0e570add74c7675cd35383a98c826ec69c93aa8d
Alex Visconde <andres.visconde@gmail.com>
2021-09-01 15:57:52 -0300

    fixing heroku react problems

--- app/assets/javascripts/rPSFriend.js
+++ app/assets/javascripts/rPSFriend.js
@@ -3,7 +3,7 @@ window.addEventListener("click", buttons)
 
 function buttons() {
 
-  let button = document.getElementsByTagName('button');
+  var button = document.getElementsByTagName('button');
 
   if (button[1]) {
     button[1].remove()
