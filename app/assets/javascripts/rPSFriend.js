14ab2a3afccb1334b31ad355444560015711f3b4
Alex Visconde <andres.visconde@gmail.com>
2021-09-01 12:01:17 -0300

    changes variable in js for probnles loading in heroku

--- app/assets/javascripts/rPSFriend.js
+++ app/assets/javascripts/rPSFriend.js
@@ -3,8 +3,8 @@
 window.addEventListener("click", buttons)
 
 function buttons() {
-  let buttons = document.getElementsByTagName('button');
-  if (buttons.length !== 0) {
-    buttons[1].remove()
+  let button = document.getElementsByTagName('button');
+  if (button.length !== 0) {
+    button[1].remove()
   }
 }
